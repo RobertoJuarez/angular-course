fb4d1655b7deb15ad38643aa4884c7d2c6c57e33
Roberto Juárez Maldonado <roberto.juarezmaldonado@soprasteria.com>
2018-01-03 11:54:08 +0100

    slide 42

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -2,10 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { ShoppingComponent } from './shopping/shopping.component';
+import { ShoppingEditComponent } from './shopping/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingComponent,
+    ShoppingEditComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule
