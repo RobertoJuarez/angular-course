957e46f059628c1559755fe1ae12a012d61aca0f
Roberto Juárez Maldonado <roberto.juarezmaldonado@soprasteria.com>
2018-01-09 13:47:29 +0100

    slide 73

--- course-project/src/app/directives/highlight.directive.ts
+++ course-project/src/app/directives/highlight.directive.ts
@@ -1,15 +1,49 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective implements OnInit {
 
-  constructor( private elementRef: ElementRef ) {}
+
+  private _hoverColor: string;
+
+  @HostBinding( 'style.backgroundColor' ) private _backgroundColor: string;
+
+
+  constructor() {
+
+    this._hoverColor = 'green';
+  }
+
 
   ngOnInit() {
+  }
 
-    this.elementRef.nativeElement.style.backgroundColor = 'green';
+
+  @Input()
+  get hoverColor() {
+    return this._hoverColor;
+  }
+
+
+  set hoverColor( value: string ) {
+
+    this._hoverColor = value;
+  }
+
+
+  @HostListener( 'mouseenter' )
+  public handleMouseEnter( event: Event ) {
+
+    this._backgroundColor = this._hoverColor;
+  }
+
+
+  @HostListener( 'mouseleave' )
+  public handleMouseLeave( event: Event ) {
+
+    this._backgroundColor = 'transparent';
   }
 
 }
